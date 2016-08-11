4e43cb054456e3c4037fd6a7e31b5a91c00438cf
Alexandre Rieux <alexr.3165@gmail.com>
2016-08-11 10:32:43 +0100

    Fixe some types issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Redux from 'redux';
 import * as ReactRedux from 'react-redux';
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react-router-redux/index.d.ts" />
+/// <reference path="globals/react-router/history/index.d.ts" />
 /// <reference path="globals/react-router/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/redux-thunk/index.d.ts" />
