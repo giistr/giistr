bb2b113a2e47859f1f28f3b3f3500bb3e4e107a2
Alexandre Rieux <alexr.3165@gmail.com>
2016-08-24 23:38:24 +0200

    Add proxy server for CORS issue using github oauth

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var fallback = require('express-history-api-fallback');
 var express = require('express');
+var config = require('./config.json');
+var bodyParser = require('body-parser');
+
+// Express setup
 var app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 var port = 3001;
 var rootDirectory = __dirname;
@@ -11,16 +19,46 @@ app.use(fallback('index.html', {
   root: rootDirectory
 }));
 
-app.get(function (req, res, next) {
+app.get("/", function handler(req, res, next) {
   if (req.accepts('html')) {
-    res.sendFile(rootDirectory + '/index.html')
+    res.sendFile(rootDirectory + '/index.html');
   } else {
-    next()
+    next();
   }
 });
 
-var server = app.listen(port, function () {
-  var port = server.address().port;
+// Proxy to get the API token from github avoiding annoying CORS issue
+app.post('/api/github-login', function handler(req, res) {
+  var code = req.body.code;
 
-  console.log('Server listening on port : ', port);
+  if (!code) {
+    res.send(500, { error: 'Provide a code' });
+  }
+
+  var form = {
+    client_id: config.clientId,
+    client_secret: config.clientSecret,
+    code: code
+  };
+
+  request
+    .post({
+      url: 'https://github.com/login/oauth/access_token',
+      form: form
+    }, function handler(err, httpResponse, body){
+      if (!error && response.statusCode === 200) {
+        var parsedRes = JSON.parse(body);
+        res.send(200, {
+          token: parsedRes.access_token
+        });
+      } else {
+        res.send(500, { error: err });
+      }
+    });
+});
+
+// Start listening on the given port
+var server = app.listen(port, function handler() {
+  var port = server.address().port;
+  console.log('Server listening on port: ', port);
 });
