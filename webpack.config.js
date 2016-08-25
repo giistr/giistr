d9e948d578e70b756179a4fafb0e0e0b80c7c793
Alexandre Rieux <alexr.3165@gmail.com>
2016-08-25 17:32:22 +0200

    Improve build process, put nginx conf in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 var env = process.env.NODE_ENV;
 
 const entries = [ 'whatwg-fetch', path.join(__dirname, 'src/App') ];
@@ -8,10 +10,16 @@ const output = {
   filename: 'bundle.js',
   path: path.join(__dirname, 'dist')
 };
+var toCopy = [
+  { from: 'assets', to: 'assets' },
+  { from: 'index.html' },
+  { from: 'nginx.conf' }
+];
 
 if (env === 'dev') {
   entries.concat([ 'webpack/hot/only-dev-server', 'webpack-dev-server/client?http://localhost:3001' ]);
   output.path = __dirname;
+  toCopy = [];
 }
 
 module.exports = {
@@ -51,6 +59,7 @@ module.exports = {
         'NODE_ENV': "'" + env + "'"
       }
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyWebpackPlugin(toCopy)
   ]
 };
